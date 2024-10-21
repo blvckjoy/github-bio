cb5d14264820bfcaf27b770a39e275e92d62e960
blvckjoy <sirjeggz@gmail.com>
2024-10-21 18:15:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # github-bio
+
+A simple web application that allows users to view GitHub profiles by entering a username. The application fetches user data from the GitHub API and displays the user's avatar, name, bio, followers, and following count.
+
+## Features
+
+-  Fetch and display GitHub user profiles.
+-  Show user avatar, name, and bio.
+-  Format followers and following counts for better readability.
+
+## Technologies Used
+
+-  HTML
+-  CSS
+-  JavaScript
+-  GitHub API
+
+## Usage
+
+1. Enter a GitHub username in the input field.
+2. Click the "Search" button to fetch the profile.
+3. The user's information will be displayed below the input field.
---
+++ script.js
@@ -0,0 +1,64 @@
+const userInfo = document.querySelector('.user-info');
+const searchBtn = document.querySelector('button');
+const avatar = document.querySelector('img');
+const followers = document.querySelector('.followers-count');
+const following = document.querySelector('.following-count');
+
+async function fetchGithubProfile() {
+   try {
+      const input = getInputValue();
+      const url = `https://api.github.com/users/${input}`;
+      validateInput(input);
+
+      const data = await fetchData(url);
+      updateProfile(data);
+      userInfo.classList.toggle('hidden');
+      userInfo.classList.add('fade-in');
+      setTimeout(() => userInfo.classList.remove('fade-in'), 500);
+   } catch (error) {
+      alert('Error:', error);
+   }
+}
+
+// function to get input value
+function getInputValue() {
+   return document.querySelector('input').value.trim();
+}
+
+// function to validate input
+function validateInput(input) {
+   if (!input) {
+      throw new Error('Uh Oh');
+   }
+}
+
+// function to fetch data
+async function fetchData(url) {
+   const res = await fetch(url);
+   return await res.json();
+}
+
+// function to update profile
+function updateProfile(data) {
+   avatar.src = `${data.avatar_url}`;
+   document.querySelector('.heading').textContent = `${data.name}`;
+   document.querySelector('.description').textContent =
+      data.bio === null ? '' : `${data.bio}`;
+   // format followers count
+   followers.textContent =
+      data.followers >= 1000
+         ? `${(data.followers / 1000).toFixed(1)}k`
+         : `${data.followers} `;
+
+   // format following count
+   following.textContent =
+      data.following >= 1000
+         ? `${(data.following / 1000).toFixed(1)}k`
+         : `${data.following} `;
+}
+
+searchBtn.addEventListener('click', (e) => {
+   e.preventDefault();
+   fetchGithubProfile();
+   document.querySelector('input').value = '';
+});
